c58215a7c151c9a623507c340b2aa9687a088d17
DeandryzidanAgustia <zidandry77@gmail.com>
2026-01-29 23:14:01 +0700

    Fix auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -14,6 +14,6 @@ module.exports = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Token tidak valid" });
+    res.status(401).json({ message: "Token tidak valid" });
   }
 };
